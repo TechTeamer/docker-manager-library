a42c038d4ec363db4630b4eea312ea5cfd1ac5ab
Patrik Molnar <xenon.hungary@gmail.com>
2024-08-16 14:33:14 +0200

    fix: attempt to fix release

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -3,6 +3,8 @@ export default {
   tagFormat: 'release-v${version}',
   plugins: [
     '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git'
   ],
   hooks: {
     parseTag: (tag) => tag.replace(/^release-v/, '')
