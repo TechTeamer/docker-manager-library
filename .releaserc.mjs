3aa9fab1e0578315b2c6d031706e832bcb576299
Patrik Molnar <xenon.hungary@gmail.com>
2024-08-16 18:37:51 +0200

    feat: readded gh related plugins

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -2,6 +2,14 @@ export default {
   branches: ['master'],
   tagFormat: 'release-v${version}',
   plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
     '@semantic-release/npm',
     '@semantic-release/github',
     '@semantic-release/git'
